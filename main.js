f1ae7f4a899908c2b8c1266ea08b5b49dfe70a6e
Jacob Collins <jacob.edward.collins@gmail.com>
2021-02-16 13:44:50 -0500

    Add looping elements

--- main.js
+++ main.js
@@ -5,9 +5,14 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+for (i =1; i < 6; i++) {
+    let pic = ('images/pic' + i +'.jpg')
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src' ,pic);
+    thumbBar.appendChild(newImage);
+    newImage.onclick = function changePic() {
+        displayedImage.src = pic;
+    }
+}
 
 /* Wiring up the Darken/Lighten button */
